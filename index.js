6432f08d60ea47e5fb64dd7c4baa8b8b7e45ec29
rigwild <contact@asauvage.fr>
2019-02-11 23:57:16 +0100

    Removed dot-env in for now secrets + better error handling

--- index.js
+++ index.js
@@ -1,45 +1,52 @@
 'use strict'
 
-require('dotenv-safe').config()
-
-const { json, createError } = require('micro')
+const { json, createError, sendError } = require('micro')
 const fetch = require('node-fetch')
 
+const endpoint = process.env.email_microservice_endpoint
+const token = process.env.email_microservice_token
+const to = process.env.email_to
+const subject = process.env.email_subject
+
 module.exports = async (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Content-Type', 'application/json')
+  try {
+    if (![endpoint, token, to, subject].every(x => x))
+      throw createError(500, 'Missing env variables.')
 
-  // Check POST method
-  if (req.method !== 'POST') throw createError(400, 'Invalid HTTP request method.')
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Content-Type', 'application/json')
 
-  // Get JSON body
-  const body = await json(req).catch(() => {
-    throw createError(400, 'Invalid JSON was sent.')
-  })
+    // Check POST method
+    if (req.method !== 'POST') throw createError(400, 'Invalid HTTP request method.')
 
-  // Get body parameters and check if set
-  const { name, email, message } = body
-  if (![name, email, message].every(x => x)) throw createError(400, 'Missing body parameters.')
-
-  const endpoint = process.env.emailMicroserviceEndpoint
-  const token = process.env.emailMicroserviceToken
-  const to = process.env.emailTo
-  const subject = process.env.emailSubject
-
-  return await fetch(endpoint, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      token,
-      from: {
-        name,
-        address: email
-      },
-      to,
-      subject,
-      content: message
+    // Get JSON body
+    const body = await json(req).catch(() => {
+      throw createError(400, 'Invalid JSON was sent.')
     })
-  }).then(res => res.json())
+
+    // Get body parameters and check if set
+    const { name, email, message } = body
+    if (![name, email, message].every(x => x)) throw createError(400, 'Missing body parameters.')
+
+    const result = await fetch(endpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        token,
+        from: {
+          name,
+          address: email
+        },
+        to,
+        subject,
+        content: message
+      })
+    }).then(res => res.json())
+
+    res.end(JSON.stringify(result))
+  } catch (err) {
+    sendError(req, res, err)
+  }
 }
